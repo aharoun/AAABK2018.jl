31335624e6a6db581695c0cd02a4fc4ecf190366
Harun Alp <harun.alp@gmail.com>
2019-02-28 18:42:58 -0500

    change printing optimal policy results

--- src/optimalPolicy.jl
+++ src/optimalPolicy.jl
@@ -14,7 +14,7 @@ function policy_opt(p ;sⁱ = false, sᶠ = false, sᴱ = false)
 
   welfMax = [calWelfare(p, eq)]	
   
-  @printf("Baseline Welfare is %3.4f \n\n", welfMax[1])
+  @printf("Baseline Welfare is %3.4f \n", welfMax[1])
   
   opt = Opt(:LN_NELDERMEAD, npol)
   ftol_rel!(opt,1e-8)
@@ -42,13 +42,22 @@ function policy_obj(polguess, poltype, p, eqInit, welfMax)
                 sⁱ = polval[1], sᶠ = polval[2], sᴱ = polval[3])
   
   eq, res = solveBGP(pNew, eqInit)
-
-  eqInit .= res.zero
-
-  # welfare calculation
-  welf  = (1.0/(1.0-p.σ))*((eq.cactivtot^((1.0 - p.σ)/(p.ε-1.0))/(p.ρ-(1.0-p.σ)*eq.g))-(1.0/p.ρ))
+  
+  if res.f_converged		
+  	eqInit .= res.zero
+  	# welfare calculation
+  	welf = (1.0/(1.0-p.σ))*((eq.cactivtot^((1.0 - p.σ)/(p.ε-1.0))/(p.ρ-(1.0-p.σ)*eq.g))-(1.0/p.ρ))
+  else
+    welf = -Inf
+  end 
   if welf>welfMax[1]
-  	@printf("Welfare improved to %3.4f (sⁱ = %2.2f, sᶠ = %2.2f, sᴱ = %2.2f)\n", welf, pNew.sⁱ, pNew.sᶠ, pNew.sᴱ)
+  	@printf("Welfare improved to %3.4f (", welf)
+	poltype[1] ? @printf("sⁱ = %2.2f", pNew.sⁱ) : nothing
+	poltype[1]&(poltype[2] | poltype[3]) ? @printf(",") : nothing	
+	poltype[2] ? @printf("sᶠ = %2.2f", pNew.sᶠ) : nothing
+	poltype[2]&poltype[3] ? @printf(",") : nothing	 
+	poltype[3] ? @printf("sᴱ = %2.2f", pNew.sᴱ) : nothing
+    @printf(")\n") 
 	welfMax[1] = welf
   end
   return -welf
